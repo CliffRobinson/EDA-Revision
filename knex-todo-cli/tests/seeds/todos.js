8aa1945a36fa0b6ed0f3e450bef885a415f37eba
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-10-29 19:29:04 +1300

    more more tests

--- knex-todo-cli/tests/seeds/todos.js
+++ knex-todo-cli/tests/seeds/todos.js
@@ -1,10 +1,14 @@
+const sampleData = [
+  {id: 1, task: 'Acquire wombats.', completed:0},
+  {id: 2, task: '???', completed:0},
+  {id: 3, task: 'Profit.', completed:0}
+]
+
+exports.sample = sampleData
+
 exports.seed = function (knex) {
   return knex('todos').del()
     .then(function () {
-      return knex('todos').insert([
-        {id: 1, task: 'Acquire wombats.'},
-        {id: 2, task: '???'},
-        {id: 3, task: 'Profit.'}
-      ])
+      return knex('todos').insert(sampleData)
     })
 }
--- knex-todo-cli/tests/todos.test.js
+++ knex-todo-cli/tests/todos.test.js
@@ -1,6 +1,6 @@
 const testEnv = require('./test-environment')
 const todos = require('../todo')
-
+const sample = require("./seeds/todos").sample
 let testDb = null
 
 // Create a separate in-memory database before each test.
@@ -29,6 +29,14 @@ test('getAll returns three todos', () => {
 
       // Assert
       expect(actual).toBe(expected)
+      expect(results).toEqual(sample);
+    })
+})
+
+test('refreshTodos returns all tasks', ()=> {
+  return todos.refreshTodos(testDb)
+    .then( results => {
+      expect(results).toEqual(sample);
     })
 })
 
@@ -48,3 +56,35 @@ test('deleteById removes a DB entry', ()=> {
 })
 
 //probs better to look for thing and find it isn't there than verify that the remaining items are as they should be. 
+
+test("updateById changes task", ()=> {
+  const newTask = "sell wombat pelts"
+  const {getAll} = todos; 
+  return todos.updateById(2, newTask, testDb)
+    .then( (results) => {
+      expect(results).toEqual(1);
+      return getAll(testDb)
+        .then( (content) => {
+          expect(content[1].task).toEqual(newTask);
+        })
+    })
+})
+
+test("searchByString returns middle entry", ()=> {
+  return todos.searchByString("?", testDb)
+    .then((results) => {
+      expect(results).toEqual([sample[1]])
+    })
+})
+
+test("completeById creates true values", ()=> {
+  const {getAll} = todos; 
+  return todos.completeById(1, testDb)
+  .then( (results) => {
+    expect(results).toEqual(1)
+    return getAll(testDb)
+    .then( (content) => {
+      expect(content[0].completed).toBeTruthy()
+    })
+  })
+})
